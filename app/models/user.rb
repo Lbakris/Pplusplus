2c6fe65ab6877c161bb5d91eed792fc4b7a2409b
Bradford Loesch <bradford.loesch@gmail.com>
2016-11-22 17:31:29 -0600

    validation changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :player2s, :class_name => "Match"
   has_many :winners, :class_name => "Match"
 
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :first_name, :last_name, presence: true, length: {minimum: 2}
   validates :pin, numericality: {only_integer: true, greater_than: 999, less_than: 10000}, uniqueness: true
+  validates :email, uniqueness: {case_sensitive: false}, format: {with: EMAIL_REGEX}
 end
